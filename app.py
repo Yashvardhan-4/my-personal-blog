8fb3b5d96414579e28a27d1a2a68a8af80c5d75f
Yashvardhan Borude <yashvardhanborude7@gmail.com>
2025-08-29 01:16:15 +0530

    Added /create

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask , render_template,redirect,request,url_for
 
 app = Flask(__name__)
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_post = Post(title=title, content=content)
+        
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect(url_for('hello_world'))
+        except:
+            return 'There was an issue adding your post'
+    else:
+        return render_template('create.html')
+from flask_sqlalchemy import SQLAlchemy
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_blog.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+    
 @app.route('/')
 def hello_world():
-    return 'My First Blog!'
+    return render_template('index.html')
+
+@app.route('/about')
+def about () :
+    return 'This is the about page.'
 
 if __name__ == '__main__':
     app.run(debug=True)
